6532541447f921525fde8605f2443304cf599603
Robb Alexander <RobbMAlexander@gmail.com>
2022-01-20 23:57:45 -0800

    took working API call code and constructors, made modular. Refactoring breaks API key checks as-is

---
+++ movies.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const axios = require('axios');
+
+class MovieDisplay {
+  constructor(movie) {
+    this.title = movie.title;
+    this.description = movie.overview;
+    this.src = movie.poster_path;
+  }
+}
+
+function getMovies(req, res) {
+
+  let cityQuery = req.query.query;
+
+  // let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_MOVIE_API_KEY}&language=en-US&query=${cityQuery}&page=1&include_adult=false`;
+
+  let url = `https://api.themoviedb.org/3/search/movie`;
+
+  let parameters = {
+    key: process.env.REACT_APP_MOVIE_API_KEY,
+    language: 'en-US',
+    query: cityQuery,
+    page: 1,
+    include_adult: false,
+  };
+
+  // let receivedMoviesAPI = await axios.get(url);
+
+  axios.get(url, { parameters })
+    .then((receivedMoviesAPI) => {
+      (receivedMoviesAPI.data.results.map(movie => new MovieDisplay(movie)))
+        .then(groomedMovieData => res.send(groomedMovieData))
+        .catch(err => console.error(err));
+    });
+}
+module.exports = getMovies;
---
+++ weather.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const axios = require('axios');
+
+class WeatherDisplay {
+  constructor(day) {
+    this.date = day.valid_date;
+    this.dayHigh = day.high_temp;
+    this.dayLow = day.low_temp;
+    this.weatherDescription = day.weather.description;
+  }
+}
+
+function getWeather(req, res) {
+  let lat = req.query.lat;
+  let lon = req.query.lon;
+
+  let url = `https://api.weatherbit.io/v1.0/forecast/daily`;
+
+  let parameters = {
+    key: process.env.REACT_APP_WEATHER_API_KEY,
+    lat: lat,
+    lon: lon,
+    units: 'I',
+  };
+
+  // let receivedWeatherAPI = await axios.get(url);
+
+  axios.get(url, { parameters })
+    .then((receivedWeatherAPI) => {
+      (receivedWeatherAPI.data.data.map(day => new WeatherDisplay(day)))
+        .then(groomedWeatherData => res.send(groomedWeatherData))
+        .catch(err => console.error(err));
+    });
+}
+
+module.exports = getWeather;
